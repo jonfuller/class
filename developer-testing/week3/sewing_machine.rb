210b1c6baa500af5df7430fb1432c2875e07575e
Jon Fuller <fullerjc@gmail.com>
2010-04-12 11:18:00 -0400

    add test for table size

--- developer-testing/week3/sewing_machine.rb
+++ developer-testing/week3/sewing_machine.rb
@@ -1,2 +1,20 @@
 class SewingMachine
+  attr_accessor :table_size
+
+  def initialize(table_size)
+    @table_size = table_size
+  end
+end
+
+class Size
+  attr_accessor :width
+  attr_accessor :height
+
+  def initialize(height=0, width=0)
+    @height, @width = height, width
+  end
+
+  def to_s
+    "[width='#{@width}', height='#{height}']"
+  end
 end
--- developer-testing/week3/sewing_machine_spec.rb
+++ developer-testing/week3/sewing_machine_spec.rb
@@ -1,15 +1,17 @@
 require 'sewing_machine'
+require 'spec_helper'
 
 describe SewingMachine do
-  context '()' do
+  context '(when machine is initialized)' do
     before(:each) do
-      @machine = SewingMachine.new
+      @machine = SewingMachine.new(Size.new(10, 10))
     end
 
-    it '' do
+    it 'should report table size' do
+      @machine.table_size.should be_of_size({:width=>10, :height=>10})
     end
 
 
       #lambda { @machine.whatever }.should raise_exception(WhateverException)
   end
-
+end
