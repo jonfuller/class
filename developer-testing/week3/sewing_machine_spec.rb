79232cd8d53dd1d9f8e3662c05a11c6ba6c4d09a
Jon Fuller <fullerjc@gmail.com>
2010-04-12 10:41:47 -0400

    added stub files

---
+++ developer-testing/week3/sewing_machine_spec.rb
@@ -0,0 +1,15 @@
+require 'sewing_machine'
+
+describe SewingMachine do
+  context '()' do
+    before(:each) do
+      @machine = SewingMachine.new
+    end
+
+    it '' do
+    end
+
+
+      #lambda { @machine.whatever }.should raise_exception(WhateverException)
+  end
+
