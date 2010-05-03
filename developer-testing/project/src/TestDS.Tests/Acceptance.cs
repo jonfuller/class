0a92dade4bd6878697e72ce4fe22fb7c63a8c11d
Jon Fuller <fullerjc@gmail.com>
2010-05-03 14:35:24 -0400

    updating mspec to .net4

--- developer-testing/project/src/TestDS.Tests/Acceptance.cs
+++ developer-testing/project/src/TestDS.Tests/Acceptance.cs
@@ -1,6 +1,18 @@
-﻿namespace TestDS.Tests
+﻿using System;
+using System.IO;
+using Machine.Specifications;
+
+namespace TestDS.Tests
 {
+    [Subject("The Thing")]
     public class Acceptance
     {
+        static Exception Exception;
+
+        Because of = () =>
+            Exception = Catch.Exception(() => { throw new Exception(); });
+
+        It should_fail = () =>
+            Exception.ShouldBeOfType<Exception>();
     }
 }
