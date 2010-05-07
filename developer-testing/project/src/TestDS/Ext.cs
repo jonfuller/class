231d79d8309265ec4cf171ea0e33198d0e40f6be
Jon Fuller <fullerjc@gmail.com>
2010-05-07 16:47:23 -0400

    some extension methods

---
+++ developer-testing/project/src/TestDS/Ext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestDS
+{
+    public static class Ext
+    {
+        public static IEnumerable<T> Each<T>(this IEnumerable<T> target, Action<T> action)
+        {
+            foreach(var item in target)
+                action(item);
+            return target;
+        }
+
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return string.Format(format, args);
+        }
+    }
+}
