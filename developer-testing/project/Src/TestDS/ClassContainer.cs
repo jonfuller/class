7b19702d8c05c542b103b47fa3ff122aca78f249
Jon Fuller <fullerjc@gmail.com>
2010-05-14 08:20:23 -0400

    split class test case into it's own file

--- developer-testing/project/Src/TestDS/ClassContainer.cs
+++ developer-testing/project/Src/TestDS/ClassContainer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 
 namespace TestDS
 {
@@ -37,39 +36,6 @@ namespace TestDS
         public string Name
         {
             get { return _type.Name; }
-        }
-    }
-
-    public class ClassTestCase : ITestCase
-    {
-        private readonly Type _type;
-        private readonly MethodInfo _methodInfo;
-
-        public ClassTestCase(Type type, MethodInfo methodInfo)
-        {
-            _type = type;
-            _methodInfo = methodInfo;
-        }
-
-        public bool Run()
-        {
-            try
-            {
-                var testObj = Activator.CreateInstance(_type);
-                _methodInfo.Invoke(testObj, new object[] { });
-                return true;
-            }
-            catch (TargetInvocationException e)
-            {
-                if (e.InnerException != null && e.InnerException is AssertionException)
-                    return false;
-                throw;
-            }
-        }
-
-        public string Name
-        {
-            get { return _methodInfo.Name; }
         }
     }
 }
---
+++ developer-testing/project/src/TestDS/ClassTestCase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace TestDS
+{
+    public class ClassTestCase : ITestCase
+    {
+        private readonly Type _type;
+        private readonly MethodInfo _methodInfo;
+
+        public ClassTestCase(Type type, MethodInfo methodInfo)
+        {
+            _type = type;
+            _methodInfo = methodInfo;
+        }
+
+        public bool Run()
+        {
+            try
+            {
+                var testObj = Activator.CreateInstance(_type);
+                _methodInfo.Invoke(testObj, new object[] { });
+                return true;
+            }
+            catch (TargetInvocationException e)
+            {
+                if (e.InnerException != null && e.InnerException is AssertionException)
+                    return false;
+                throw;
+            }
+        }
+
+        public string Name
+        {
+            get { return _methodInfo.Name; }
+        }
+    }
+}
