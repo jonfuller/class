dd7b8846557cd5c46d0299099c0c6daabbdfca78
Jon Fuller <fullerjc@gmail.com>
2010-05-14 17:18:30 -0500

    starting to add some assertions

---
+++ developer-testing/project/Src/TestDS.Tests/Assertions/ShouldEqualSpecs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Machine.Specifications;
+using TestDS.Assertion;
+
+namespace TestDS.Tests.Assertions
+{
+    [Subject("Equality Assertions")]
+    public class comparing_equal_integers : AssertionSpecs
+    {
+        Because of = () =>
+            RunAssertion(() => AssertExt.ShouldEqual(1, 1));
+
+        It should_pass = () =>
+            passed.ShouldBeTrue();
+    }
+
+    [Subject("Equality Assertions")]
+    public class comparing_nonequal_integers : AssertionSpecs
+    {
+        Because of = () =>
+            RunAssertion(() => AssertExt.ShouldEqual(1, 2));
+
+        It should_fail = () =>
+            passed.ShouldBeFalse();
+
+        It should_show_expected_and_actual_values = () =>
+            message.ShouldContain("Expected <2> but got <1>");
+    }
+
+    [Subject("Non-Nullity Assertions")]
+    public class asserting_null_value_as_not_null : AssertionSpecs
+    {
+        Because of = () =>
+            RunAssertion(() => AssertExt.ShouldNotBeNull(null));
+
+        It should_fail = () =>
+            passed.ShouldBeFalse();
+
+        It should_show_expected_and_actual_values = () =>
+            message.ShouldContain("Expected <not null> but got <null>");
+    }
+
+    [Subject("Non-Nullity Assertions")]
+    public class asserting_value_as_not_null : AssertionSpecs
+    {
+        Because of = () =>
+            RunAssertion(() => AssertExt.ShouldNotBeNull(3));
+
+        It should_pass = () =>
+            passed.ShouldBeTrue();
+    }
+
+    public abstract class AssertionSpecs
+    {
+        protected static string message { get { return thrown.Message; } }
+        protected static bool passed { get { return thrown == null; } }
+
+        static Exception thrown;
+
+        protected static void RunAssertion(Action assertion)
+        {
+            thrown = Catch.Exception(assertion);
+        }
+    }
+}
---
+++ developer-testing/project/Src/TestDS/Assertion/AssertExt.cs
@@ -0,0 +1,19 @@
+﻿namespace TestDS.Assertion
+{
+    public static class AssertExt
+    {
+        public static void ShouldEqual<T>(this T actual, T expected)
+        {
+            if (actual.Equals(expected))
+                return;
+            throw new AssertionException("Not Equal.  Expected <{0}> but got <{1}>".FormatWith(expected, actual));
+        }
+
+        public static void ShouldNotBeNull(this object actual)
+        {
+            if (actual != null)
+                return;
+            throw new AssertionException("Is null.  Expected <not null> but got <null>");
+        }
+    }
+}
